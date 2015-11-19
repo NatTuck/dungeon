7fb00f80a15736105d018ac573164b8b73903fe6
Nat Tuck <nat@ferrus.net>
2015-11-19 16:14:31 -0500

    TODO: Figure out if I want "Bower".

--- web/channels/link_channel.ex
+++ web/channels/link_channel.ex
@@ -3,7 +3,7 @@ defmodule Dungeon.LinkChannel do
 
   def join("links:" <> link_id, payload, socket) do
     if authorized?(payload) do
-      IO.puts "Join #{link_id}"
+      socket = assign(socket, :user_id, link_id)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -12,8 +12,14 @@ defmodule Dungeon.LinkChannel do
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_in("pull", _payload, socket) do
+    {:ok, pid} = Dungeon.PlayerSup.get(socket[:user_id])
+    {:ok, plr} = Dungeon.PlayerSv.get(pid) 
+
+    {:ok, rpid} = Dungeon.RoomSup.get(plr[:room].id)
+    {:ok, room} = Dungeon.Room.get(rpid)
+
+    {:reply, {:ok, "derp"}, socket}
   end
 
   # It is also common to receive messages from the client and
@@ -28,7 +34,7 @@ defmodule Dungeon.LinkChannel do
     html = Phoenix.View.render_to_string(Dungeon.PlayView, "game.html", [date: date])
     broadcast!(socket, "show", %{game_html: html})
 
-    {:ok, pid} = Dungeon.PlayerBoss.get(link_id)
+    {:ok, pid} = Dungeon.PlayerSup.get(link_id)
     Dungeon.Player.poke(pid)
 
     {:noreply, socket}
